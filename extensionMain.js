48746a619e52904dc339af433754f0e16430fffc
Andreas Törnkvist <andreas.o.tornkvist@gmail.com>
2019-02-01 20:07:54 +0100

    Update extensionMain.js

--- extensionMain.js
+++ extensionMain.js
@@ -1,20 +1,22 @@
 var changeData = [
-  ["gui_menu-bar-position_3U1T0", "#21252b"],
-  ["gui_body-wrapper_-N0sA", "#282c34"],
-  ["blocklyToolboxDiv", "#21252b"],
-  ["scratchCategoryMenu", "#21252b"],
-  ["sprite-selector_sprite-selector_2KgCX", "#3a3f4b"],
-  ["sprite-info_sprite-info_3EyZh", "#21252b"],
-  ["stage-selector_stage-selector_3oWOr", "#3a3f4b"],
-  ["stage-selector_header_2GVr1", "#21252b"],
-  ["gui_tab_27Unf", "#21252b"],
-  ["blocklySvg", "#3a3f4b"]
+  ["gui_menu-bar-position_3U1T0", "#21252b", "bgColor"],
+  ["gui_body-wrapper_-N0sA", "#282c34", "bgColor"],
+  ["blocklyToolboxDiv", "#21252b", "bgColor"],
+  ["scratchCategoryMenu", "#21252b", "bgColor"],
+  ["sprite-selector_sprite-selector_2KgCX", "#3a3f4b", "bgColor"],
+  ["sprite-info_sprite-info_3EyZh", "#21252b", "bgColor"],
+  ["stage-selector_stage-selector_3oWOr", "#3a3f4b", "bgColor"],
+  ["stage-selector_header_2GVr1", "#21252b", "bgColor"],
+  ["gui_tab_27Unf", "#21252b", "bgColor"],
+  ["blocklySvg", "#3a3f4b", "bgColor"]
 ]
 
 for (i=0; i < changeData.length; i++){
   var elems = document.getElementsByClassName(changeData[i][0]);
   for (var ii = 0; ii < elems.length; ii++) {
-    elems[ii].style.background = changeData[i][1];
+    if(changeData[i][2] == "bgColor"){
+      elems[ii].style.background = changeData[i][1];
+    }
   }
 }
 
